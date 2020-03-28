5263159d5d2348fc125a209a089b99aeecee860e
Richard O. Gregory <richardgrecoson@gmail.com>
2020-03-28 12:06:06 +0100

    Configure Graphql and Apollo dependencies.

--- src/index.js
+++ src/index.js
@@ -3,19 +3,38 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import {ApolloProvider} from "react-apollo";
+import {createHttpLink} from "apollo-link-http";
+import {InMemoryCache} from "apollo-cache-inmemory";
+import {ApolloClient, gql} from "apollo-boost";
 
 import { store, persistor } from './redux/store';
 
 import './index.css';
 import App from './App';
 
+// establish connection to backend
+// createHttpLink is a fxn that takes an obj as arg
+const httpLink = createHttpLink({
+  uri: "https://crwn-clothing.com"
+});
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache
+})
+
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <PersistGate persistor={persistor}>
-        <App />
-      </PersistGate>
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
+  <ApolloClient client={client}>
+    <Provider store={store}>
+      <BrowserRouter>
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
+      </BrowserRouter>
+    </Provider>
+  </ApolloClient>,
+  document.getElementById("root")
 );
